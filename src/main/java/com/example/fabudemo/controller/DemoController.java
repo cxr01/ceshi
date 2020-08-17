981d48ce34211dad2b22df2abb2880fb91cb9037
cxr <m13663485663@163.com>
2020-08-17 17:16:50 +0800

    11

--- src/main/java/com/example/fabudemo/controller/DemoController.java
+++ src/main/java/com/example/fabudemo/controller/DemoController.java
@@ -11,7 +11,7 @@ public class DemoController {
     public String login(){
         System.out.println("aaaaa01");
         System.out.println("123");
-        return "jenkins测试成功！！！";
+        return "jenkins测试成功！！！...";
     }
 
 
