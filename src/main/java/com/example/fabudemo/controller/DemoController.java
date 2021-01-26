e6b662841e86ace81fd866be568c6dafed0a6077
cxr <m13663485663@163.com>
2021-01-26 10:49:17 +0800

    11

--- src/main/java/com/example/fabudemo/controller/DemoController.java
+++ src/main/java/com/example/fabudemo/controller/DemoController.java
@@ -3,7 +3,6 @@ package com.example.fabudemo.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/demo")
 @RestController
 public class DemoController {
 
